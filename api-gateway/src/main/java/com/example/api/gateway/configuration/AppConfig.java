4aaeb2eaef028a978272e5334d61e8640b6655ca
rajadilipkolli <rajakolli@deloitte.com>
2021-05-01 12:26:38 +0530

    adds circuit breaker to gateway

--- api-gateway/src/main/java/com/example/api/gateway/configuration/AppConfig.java
+++ api-gateway/src/main/java/com/example/api/gateway/configuration/AppConfig.java
@@ -1,5 +1,11 @@
 package com.example.api.gateway.configuration;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import java.time.Duration;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,5 +17,19 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory ->
+                factory.configureDefault(
+                        id ->
+                                new Resilience4JConfigBuilder(id)
+                                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                                        .timeLimiterConfig(
+                                                TimeLimiterConfig.custom()
+                                                        .timeoutDuration(Duration.ofSeconds(5))
+                                                        .build())
+                                        .build());
     }
 }
