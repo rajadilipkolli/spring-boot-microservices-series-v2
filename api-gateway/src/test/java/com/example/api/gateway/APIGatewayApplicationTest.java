95648281a654ffd433e89b56245a9f7b112c14d5
Raja Dilip Kolli <rajadileepkolli@gmail.com>
2022-04-24 09:22:26 +0000

    adds missing servers

--- api-gateway/src/test/java/com/example/api/gateway/APIGatewayApplicationTest.java
+++ api-gateway/src/test/java/com/example/api/gateway/APIGatewayApplicationTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.lifecycle.Startables;
@@ -26,72 +27,86 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureWebClient
 class APIGatewayApplicationTest {
 
-    private static final int CONFIG_SERVER_INTERNAL_PORT = 8888;
-    private static final int NAMING_SERVER_INTERNAL_PORT = 8761;
+  private static final int CONFIG_SERVER_INTERNAL_PORT = 8888;
+  private static final int NAMING_SERVER_INTERNAL_PORT = 8761;
 
-    @Container
-    static NamingServerContainer namingServerContainer = new NamingServerContainer(DockerImageName.parse("dockertmt/mmv2-service-registry:0.0.1-SNAPSHOT"));
+  @Container
+  static NamingServerContainer namingServerContainer =
+      new NamingServerContainer(
+          DockerImageName.parse("dockertmt/mmv2-service-registry:0.0.1-SNAPSHOT"));
 
-    @Container
-    static ConfigServerContainer configServerContainer = new ConfigServerContainer(DockerImageName.parse("dockertmt/mmv2-config-server:0.0.1-SNAPSHOT"));
+  @Container
+  static ConfigServerContainer configServerContainer =
+      new ConfigServerContainer(
+          DockerImageName.parse("dockertmt/mmv2-config-server:0.0.1-SNAPSHOT"));
 
-    static {
-        Startables.deepStart(List.of( configServerContainer, namingServerContainer)).join();
+  @Container
+  static MongoDBContainer mongoDBContainer =
+      new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+
+  static {
+    Startables.deepStart(List.of(configServerContainer, namingServerContainer, mongoDBContainer))
+        .join();
+  }
+
+  private static class NamingServerContainer extends GenericContainer<NamingServerContainer> {
+
+    public NamingServerContainer(final DockerImageName dockerImageName) {
+      super(dockerImageName);
+      withExposedPorts(NAMING_SERVER_INTERNAL_PORT);
     }
+  }
 
-    private static class NamingServerContainer extends GenericContainer<NamingServerContainer> {
+  private static class ConfigServerContainer extends GenericContainer<ConfigServerContainer> {
 
-        public NamingServerContainer(final DockerImageName dockerImageName) {
-            super(dockerImageName);
-            withExposedPorts(NAMING_SERVER_INTERNAL_PORT);
-        }
+    public ConfigServerContainer(final DockerImageName dockerImageName) {
+      super(dockerImageName);
+      withExposedPorts(CONFIG_SERVER_INTERNAL_PORT);
     }
+  }
 
-    private static class ConfigServerContainer extends GenericContainer<ConfigServerContainer> {
+  @DynamicPropertySource
+  static void addApplicationProperties(DynamicPropertyRegistry propertyRegistry) {
 
-        public ConfigServerContainer(final DockerImageName dockerImageName) {
-            super(dockerImageName);
-            withExposedPorts(CONFIG_SERVER_INTERNAL_PORT);
-        }
-    }
+    propertyRegistry.add(
+        "spring.config.import",
+        () ->
+            String.format(
+                "optional:configserver:http://%s:%d/",
+                configServerContainer.getHost(),
+                configServerContainer.getMappedPort(CONFIG_SERVER_INTERNAL_PORT)));
+    propertyRegistry.add(
+        "eureka.client.serviceUrl.defaultZone",
+        () ->
+            String.format(
+                "http://%s:%d/eureka/",
+                namingServerContainer.getHost(),
+                namingServerContainer.getMappedPort(NAMING_SERVER_INTERNAL_PORT)));
 
-    @DynamicPropertySource
-    static void addApplicationProperties(DynamicPropertyRegistry propertyRegistry) {
+    propertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+  }
 
-        propertyRegistry.add("spring.config.import", () ->
-                String.format(
-                        "optional:configserver:http://%s:%d/",
-                        configServerContainer.getHost(),
-                        configServerContainer.getMappedPort(CONFIG_SERVER_INTERNAL_PORT)
-                )
-        );
-        propertyRegistry.add("eureka.client.serviceUrl.defaultZone", () ->
-                String.format("http://%s:%d/eureka/",
-                        namingServerContainer.getHost(),
-                        namingServerContainer.getMappedPort(NAMING_SERVER_INTERNAL_PORT)
-                )
-        );
-    }
+  @Autowired private WebTestClient webTestClient;
 
-    @Autowired
-    private WebTestClient webTestClient;
+  @Test
+  void contextLoads() {
+    assertThat(namingServerContainer.isRunning()).isTrue();
+    assertThat(configServerContainer.isRunning()).isTrue();
+    assertThat(mongoDBContainer.isRunning()).isTrue();
+  }
 
-    @Test
-    void contextLoads() {
-        assertThat(namingServerContainer.isRunning()).isTrue();
-        assertThat(configServerContainer.isRunning()).isTrue();
-    }
+  @Test
+  void testLogin() {
+    AuthenticationRequest body = new AuthenticationRequest();
+    body.setUsername("user");
+    body.setPassword("password");
 
-    @Test
-    void testLogin() {
-        AuthenticationRequest body = new AuthenticationRequest();
-        body.setUsername("user");
-        body.setPassword("password");
-
-        this.webTestClient.post()
-            .body(BodyInserters.fromValue(body))
-            .accept(MediaType.APPLICATION_JSON)
-            .exchange().expectStatus().isUnauthorized();
-    }
-
+    this.webTestClient
+        .post()
+        .body(BodyInserters.fromValue(body))
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isUnauthorized();
+  }
 }
--- service-registry/src/test/java/org/service/registry/NamingServerApplicationTests.java
+++ service-registry/src/test/java/org/service/registry/NamingServerApplicationTests.java
@@ -2,6 +2,8 @@ package org.service.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
@@ -22,15 +24,27 @@ class NamingServerApplicationTests {
     configServerContainer.start();
   }
 
-  @Test
-  void contextLoads() {
-    assertThat(configServerContainer.isRunning()).isTrue();
-  }
-
   private static class ConfigServerContainer extends GenericContainer<ConfigServerContainer> {
     public ConfigServerContainer(final DockerImageName dockerImageName) {
       super(dockerImageName);
       withExposedPorts(CONFIG_SERVER_INTERNAL_PORT);
     }
+  }
+
+  @DynamicPropertySource
+  static void addApplicationProperties(DynamicPropertyRegistry propertyRegistry) {
+
+    propertyRegistry.add(
+        "spring.config.import",
+        () ->
+            String.format(
+                "optional:configserver:http://%s:%d/",
+                configServerContainer.getHost(),
+                configServerContainer.getMappedPort(CONFIG_SERVER_INTERNAL_PORT)));
+  }
+
+  @Test
+  void contextLoads() {
+    assertThat(configServerContainer.isRunning()).isTrue();
   }
 }
