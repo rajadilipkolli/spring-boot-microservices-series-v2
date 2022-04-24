e11fc56d9839b6a38c0501f69a79598cd2208ce2
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2022-04-24 15:35:58 +0530

    using java 17 image (#156)
    * using java 17 image
    
    * fixes issue with deprecated properties
    
    * adds missing servers
    
    * adds profile properties

--- service-registry/src/test/java/org/service/registry/NamingServerApplicationTests.java
+++ service-registry/src/test/java/org/service/registry/NamingServerApplicationTests.java
@@ -2,11 +2,49 @@ package org.service.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class NamingServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  private static final int CONFIG_SERVER_INTERNAL_PORT = 8888;
+
+  @Container
+  static ConfigServerContainer configServerContainer =
+      new ConfigServerContainer(
+          DockerImageName.parse("dockertmt/mmv2-config-server:0.0.1-SNAPSHOT"));
+
+  static {
+    configServerContainer.start();
+  }
+
+  private static class ConfigServerContainer extends GenericContainer<ConfigServerContainer> {
+    public ConfigServerContainer(final DockerImageName dockerImageName) {
+      super(dockerImageName);
+      withExposedPorts(CONFIG_SERVER_INTERNAL_PORT);
+    }
+  }
+
+  @DynamicPropertySource
+  static void addApplicationProperties(DynamicPropertyRegistry propertyRegistry) {
+
+    propertyRegistry.add(
+        "spring.config.import",
+        () ->
+            String.format(
+                "optional:configserver:http://%s:%d/",
+                configServerContainer.getHost(),
+                configServerContainer.getMappedPort(CONFIG_SERVER_INTERNAL_PORT)));
+  }
+
+  @Test
+  void contextLoads() {
+    assertThat(configServerContainer.isRunning()).isTrue();
+  }
 }
