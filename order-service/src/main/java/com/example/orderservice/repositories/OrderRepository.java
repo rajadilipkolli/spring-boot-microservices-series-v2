782d464103f4d8735cec04255937daa37eac76cc
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2022-05-29 17:50:20 +0530

    update order after receiving update from payment and inventory

--- order-service/src/main/java/com/example/orderservice/repositories/OrderRepository.java
+++ order-service/src/main/java/com/example/orderservice/repositories/OrderRepository.java
@@ -4,6 +4,7 @@ import com.example.orderservice.entities.Order;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,4 +15,11 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select o from Order o join fetch o.items oi where o.id = :id")
     Optional<Order> findOrderById(@Param("id") Long id);
+
+    @Modifying
+    @Query("update Order o set o.status =:status, o.source =:source where o.id = :id")
+    int updateOrderStatusAndSourceById(
+            @Param("id") Long orderId,
+            @Param("status") String status,
+            @Param("source") String source);
 }
