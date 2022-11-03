df1c709e51857f8ae9703ba367da59a6edb73bcc
Raja Kolli <rajadilipkolli@gmail.com>
2022-11-03 16:29:30 +0530

    adds missing properties

--- inventory-service/src/main/java/com/example/inventoryservice/utils/AppConstants.java
+++ inventory-service/src/main/java/com/example/inventoryservice/utils/AppConstants.java
@@ -11,4 +11,8 @@ public final class AppConstants {
     public static final String STOCK_ORDERS_TOPIC = "stock-orders";
     public static final String SOURCE = "stock";
     public static final String ROLLBACK = "ROLLBACK";
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
 }
