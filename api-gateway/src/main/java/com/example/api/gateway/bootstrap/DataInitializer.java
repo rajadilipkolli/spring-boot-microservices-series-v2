817b34fddcbb704ce46c2475c5edcff2dccfec94
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-17 07:47:40 +0000

    Temporarily removes spring security

--- api-gateway/src/main/java/com/example/api/gateway/bootstrap/DataInitializer.java
+++ api-gateway/src/main/java/com/example/api/gateway/bootstrap/DataInitializer.java
@@ -1,61 +1,19 @@
 /* Licensed under Apache-2.0 2021-2022 */
 package com.example.api.gateway.bootstrap;
 
-import com.example.api.gateway.domain.User;
-import com.example.api.gateway.repository.UserRepository;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class DataInitializer {
 
-    private final UserRepository userRepository;
-
-    private final PasswordEncoder passwordEncoder;
-
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
         log.info("start data initialization...");
-
-        this.userRepository
-                .deleteAll()
-                .thenMany(
-                        Flux.just("user", "admin")
-                                .flatMap(
-                                        username -> {
-                                            List<String> roles =
-                                                    "user".equals(username)
-                                                            ? Collections.singletonList("ROLE_USER")
-                                                            : Arrays.asList(
-                                                                    "ROLE_USER",
-                                                                    "ROLE_ADMIN",
-                                                                    "ROLE_ACTUATOR");
-
-                                            User user =
-                                                    User.builder()
-                                                            .roles(roles)
-                                                            .username(username)
-                                                            .password(
-                                                                    passwordEncoder.encode(
-                                                                            "password"))
-                                                            .email(username + "@example.com")
-                                                            .build();
-
-                                            return this.userRepository.save(user);
-                                        }))
-                .subscribe(
-                        data -> log.info("data: {}", data),
-                        err -> log.error("error:", err),
-                        () -> log.info("done initialization..."));
     }
 }
--- api-gateway/src/test/java/com/example/api/gateway/APIGatewayApplicationTest.java
+++ api-gateway/src/test/java/com/example/api/gateway/APIGatewayApplicationTest.java
@@ -3,16 +3,13 @@ package com.example.api.gateway;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.example.api.gateway.web.AuthenticationRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -42,20 +39,5 @@ class APIGatewayApplicationTest {
     @Test
     void contextLoads() {
         assertThat(mongoDBContainer.isRunning()).isTrue();
-    }
-
-    @Test
-    void testLogin() {
-        AuthenticationRequest body = new AuthenticationRequest();
-        body.setUsername("user");
-        body.setPassword("password");
-
-        this.webTestClient
-                .post()
-                .body(BodyInserters.fromValue(body))
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus()
-                .isUnauthorized();
     }
 }
