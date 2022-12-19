04f43b220bc994f8cff924e6b91673a965308a5c
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-19 19:21:29 +0530

    adds precondition & attempt to convert to native image

--- catalog-service/src/main/java/com/example/catalogservice/config/WebMvcConfig.java
+++ catalog-service/src/main/java/com/example/catalogservice/config/WebMvcConfig.java
@@ -1,10 +1,13 @@
 package com.example.catalogservice.config;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
@@ -14,5 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowedOriginPatterns("*")
                 .allowCredentials(true);
+    }
+
+    // for AOP in native image
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
     }
 }
