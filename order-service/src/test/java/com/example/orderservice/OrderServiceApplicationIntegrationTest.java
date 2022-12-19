7814b797b679ea519f5f196c9f7d6f5069ba3f9a
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-19 12:47:24 +0530

    tweaks config and fixes junits

--- order-service/src/test/java/com/example/orderservice/OrderServiceApplicationIntegrationTest.java
+++ order-service/src/test/java/com/example/orderservice/OrderServiceApplicationIntegrationTest.java
@@ -4,7 +4,6 @@ package com.example.orderservice;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.orderservice.utils.AppConstants;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -12,7 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
 
@@ -22,26 +21,41 @@ class OrderServiceApplicationIntegrationTest {
 
     private static final int CONFIG_SERVER_INTERNAL_PORT = 8888;
     private static final int NAMING_SERVER_INTERNAL_PORT = 8761;
+    private static final int ZIPKIN_INTERNAL_PORT = 9411;
 
-    @Container
-    static NamingServerContainer namingServerContainer =
+    static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER =
+            new PostgreSQLContainer<>("postgres:15-alpine")
+                    .withDatabaseName("integration-tests-db")
+                    .withUsername("username")
+                    .withPassword("password");
+
+    static final NamingServerContainer NAMING_SERVER_CONTAINER =
             new NamingServerContainer(
-                    DockerImageName.parse("dockertmt/mmv2-service-registry-17:0.0.1-SNAPSHOT"));
+                            DockerImageName.parse(
+                                    "dockertmt/mmv2-service-registry-17:0.0.1-SNAPSHOT"))
+                    .withExposedPorts(NAMING_SERVER_INTERNAL_PORT);
 
-    @Container
-    static ConfigServerContainer configServerContainer =
+    static final ConfigServerContainer CONFIG_SERVER_CONTAINER =
             new ConfigServerContainer(
-                    DockerImageName.parse("dockertmt/mmv2-config-server:0.0.1-SNAPSHOT"));
+                            DockerImageName.parse("dockertmt/mmv2-config-server-17:0.0.1-SNAPSHOT"))
+                    .withExposedPorts(CONFIG_SERVER_INTERNAL_PORT);
+    static final ZipkinContainer ZIPKIN_CONTAINER =
+            new ZipkinContainer(DockerImageName.parse("openzipkin/zipkin-slim"))
+                    .withExposedPorts(ZIPKIN_INTERNAL_PORT);
 
     static {
-        Startables.deepStart(configServerContainer, namingServerContainer).join();
+        Startables.deepStart(
+                        CONFIG_SERVER_CONTAINER,
+                        NAMING_SERVER_CONTAINER,
+                        POSTGRE_SQL_CONTAINER,
+                        ZIPKIN_CONTAINER)
+                .join();
     }
 
     private static class NamingServerContainer extends GenericContainer<NamingServerContainer> {
 
         public NamingServerContainer(final DockerImageName dockerImageName) {
             super(dockerImageName);
-            withExposedPorts(NAMING_SERVER_INTERNAL_PORT);
         }
     }
 
@@ -49,33 +63,50 @@ class OrderServiceApplicationIntegrationTest {
 
         public ConfigServerContainer(final DockerImageName dockerImageName) {
             super(dockerImageName);
-            withExposedPorts(CONFIG_SERVER_INTERNAL_PORT);
         }
     }
 
     @DynamicPropertySource
     static void addApplicationProperties(DynamicPropertyRegistry propertyRegistry) {
-
+        propertyRegistry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
+        propertyRegistry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
+        propertyRegistry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
         propertyRegistry.add(
                 "spring.config.import",
                 () ->
                         String.format(
                                 "optional:configserver:http://%s:%d/",
-                                configServerContainer.getHost(),
-                                configServerContainer.getMappedPort(CONFIG_SERVER_INTERNAL_PORT)));
+                                CONFIG_SERVER_CONTAINER.getHost(),
+                                CONFIG_SERVER_CONTAINER.getMappedPort(
+                                        CONFIG_SERVER_INTERNAL_PORT)));
         propertyRegistry.add(
                 "eureka.client.serviceUrl.defaultZone",
                 () ->
                         String.format(
                                 "http://%s:%d/eureka/",
-                                namingServerContainer.getHost(),
-                                namingServerContainer.getMappedPort(NAMING_SERVER_INTERNAL_PORT)));
+                                NAMING_SERVER_CONTAINER.getHost(),
+                                NAMING_SERVER_CONTAINER.getMappedPort(
+                                        NAMING_SERVER_INTERNAL_PORT)));
+        propertyRegistry.add(
+                "spring.zipkin.baseurl",
+                () ->
+                        String.format(
+                                "http://%s:%d/",
+                                ZIPKIN_CONTAINER.getHost(),
+                                ZIPKIN_CONTAINER.getMappedPort(ZIPKIN_INTERNAL_PORT)));
     }
 
     @Test
-    @Disabled
     void contextLoads() {
-        assertThat(namingServerContainer.isRunning()).isTrue();
-        assertThat(configServerContainer.isRunning()).isTrue();
+        assertThat(NAMING_SERVER_CONTAINER.isRunning()).isTrue();
+        assertThat(CONFIG_SERVER_CONTAINER.isRunning()).isTrue();
+        assertThat(ZIPKIN_CONTAINER.isRunning()).isTrue();
+        assertThat(POSTGRE_SQL_CONTAINER.isRunning()).isTrue();
+    }
+
+    private static class ZipkinContainer extends GenericContainer<ZipkinContainer> {
+        public ZipkinContainer(DockerImageName dockerImageName) {
+            super(dockerImageName);
+        }
     }
 }
