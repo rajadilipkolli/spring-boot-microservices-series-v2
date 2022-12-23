6668b79ce2fba5b0c7ab1954b1d1bbdf52fbb5f9
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-23 03:34:56 +0000

    fixes formatting issue

--- order-service/src/main/java/com/example/orderservice/services/CatalogServiceProxy.java
+++ order-service/src/main/java/com/example/orderservice/services/CatalogServiceProxy.java
@@ -4,10 +4,9 @@ package com.example.orderservice.services;
 import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.service.annotation.HttpExchange;
 
 // @HttpExchange("lb://catalog-service/")
-// @HttpExchange("http://localhost:18080/catalog-service/api/catalog")
+// @HttpExchange("http://localhost:18080/catalog-service")
 // @HttpExchange(url = "${application.catalog-service-url}")
 public interface CatalogServiceProxy {
 
