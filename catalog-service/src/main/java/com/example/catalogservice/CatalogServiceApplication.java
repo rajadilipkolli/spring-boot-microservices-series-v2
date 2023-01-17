fded4d67c6adf0e0ed8154514198fc6152462a2a
Raja Dilip Kolli <rajadileepkolli@gmail.com>
2023-01-17 11:03:41 +0000

    explicitly set webapplication Type as Servlet

--- catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.catalogservice;
 
 import com.example.catalogservice.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
@@ -10,6 +11,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CatalogServiceApplication.class, args);
+        SpringApplication application = new SpringApplication(CatalogServiceApplication.class);
+        application.setWebApplicationType(WebApplicationType.SERVLET);
+        application.run(args);
     }
 }
