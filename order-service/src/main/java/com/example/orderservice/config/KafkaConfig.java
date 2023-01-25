1ab38ad03283c6ee5cdddecc65ec235c4a31e069
Raja Kolli <rajadilipkolli@gmail.com>
2023-01-25 08:49:13 +0000

    using new Method to create all topics at once

--- order-service/src/main/java/com/example/orderservice/config/KafkaConfig.java
+++ order-service/src/main/java/com/example/orderservice/config/KafkaConfig.java
@@ -7,7 +7,6 @@ import com.example.orderservice.utils.AppConstants;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
@@ -22,6 +21,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 @Configuration(proxyBeanMethods = false)
@@ -33,47 +33,40 @@ public class KafkaConfig {
     private final OrderManageService orderManageService;
 
     @Bean
-    public NewTopic ordersTopic() {
-        return TopicBuilder.name(AppConstants.ORDERS_TOPIC)
-            .partitions(2)
-            .replicas(6)
-            .compact()
-            .build();
-    }
-
-    @Bean
-    public NewTopic paymentTopic() {
-        return TopicBuilder.name(AppConstants.PAYMENT_ORDERS_TOPIC)
-            .partitions(2)
-            .replicas(6)
-            .compact()
-            .build();
-    }
-
-    @Bean
-    public NewTopic stockTopic() {
-        return TopicBuilder.name(AppConstants.STOCK_ORDERS_TOPIC)
-            .partitions(2)
-            .replicas(6)
-            .compact()
-            .build();
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name(AppConstants.ORDERS_TOPIC)
+                        .partitions(3)
+                        .replicas(1)
+                        .compact()
+                        .build(),
+                TopicBuilder.name(AppConstants.PAYMENT_ORDERS_TOPIC)
+                        .partitions(3)
+                        .replicas(1)
+                        .compact()
+                        .build(),
+                TopicBuilder.name(AppConstants.STOCK_ORDERS_TOPIC)
+                        .partitions(3)
+                        .replicas(1)
+                        .compact()
+                        .build());
     }
 
     @Bean
     public KStream<String, OrderDto> stream(StreamsBuilder builder) {
         JsonSerde<OrderDto> orderSerde = new JsonSerde<>(OrderDto.class);
         KStream<String, OrderDto> stream =
-            builder.stream(
-                AppConstants.PAYMENT_ORDERS_TOPIC,
-                Consumed.with(Serdes.String(), orderSerde));
+                builder.stream(
+                        AppConstants.PAYMENT_ORDERS_TOPIC,
+                        Consumed.with(Serdes.String(), orderSerde));
 
         stream.join(
-                builder.stream(AppConstants.STOCK_ORDERS_TOPIC),
-                orderManageService::confirm,
-                JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofSeconds(10)),
-                StreamJoined.with(Serdes.String(), orderSerde, orderSerde))
-            .peek((k, o) -> log.info("Output of Stream : {} for key :{}", o, k))
-            .to(AppConstants.ORDERS_TOPIC);
+                        builder.stream(AppConstants.STOCK_ORDERS_TOPIC),
+                        orderManageService::confirm,
+                        JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofSeconds(10)),
+                        StreamJoined.with(Serdes.String(), orderSerde, orderSerde))
+                .peek((k, o) -> log.info("Output of Stream : {} for key :{}", o, k))
+                .to(AppConstants.ORDERS_TOPIC);
 
         return stream;
     }
@@ -81,14 +74,14 @@ public class KafkaConfig {
     @Bean
     public KTable<String, OrderDto> table(StreamsBuilder builder) {
         KeyValueBytesStoreSupplier store =
-            Stores.persistentKeyValueStore(AppConstants.ORDERS_TOPIC);
+                Stores.persistentKeyValueStore(AppConstants.ORDERS_TOPIC);
         JsonSerde<OrderDto> orderSerde = new JsonSerde<>(OrderDto.class);
         KStream<String, OrderDto> stream =
-            builder.stream(
-                AppConstants.ORDERS_TOPIC, Consumed.with(Serdes.String(), orderSerde));
+                builder.stream(
+                        AppConstants.ORDERS_TOPIC, Consumed.with(Serdes.String(), orderSerde));
         return stream.toTable(
-            Materialized.<String, OrderDto>as(store)
-                .withKeySerde(Serdes.String())
-                .withValueSerde(orderSerde));
+                Materialized.<String, OrderDto>as(store)
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(orderSerde));
     }
 }
