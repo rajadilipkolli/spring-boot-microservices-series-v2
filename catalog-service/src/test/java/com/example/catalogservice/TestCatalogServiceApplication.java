3add169163ce9fe4c3c8b5d27d95242c13f6d3f0
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-22 16:45:48 +0000

    Polish testcontainers dev mode code

--- catalog-service/src/test/java/com/example/catalogservice/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/com/example/catalogservice/TestCatalogServiceApplication.java
@@ -1,14 +1,39 @@
 /*** Licensed under Apache-2.0 2023 ***/
 package com.example.catalogservice;
 
-import com.example.catalogservice.common.MyContainersConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
+@TestConfiguration(proxyBeanMethods = false)
 public class TestCatalogServiceApplication {
+
+    @Bean
+    @ServiceConnection(name = "openzipkin/zipkin")
+    GenericContainer<?> zipkinContainer() {
+        return new GenericContainer<>(DockerImageName.parse("openzipkin/zipkin"));
+    }
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSqlContainer() {
+        return new PostgreSQLContainer<>("postgres:15.3-alpine");
+    }
+
+    @Bean
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0")).withKraft();
+    }
 
     public static void main(String[] args) {
         SpringApplication.from(CatalogServiceApplication::main)
-                .with(MyContainersConfiguration.class)
+                .with(TestCatalogServiceApplication.class)
                 .run(args);
     }
 }
