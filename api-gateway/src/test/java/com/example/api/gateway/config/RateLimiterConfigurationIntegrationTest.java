e7427ed5ee9669bdff4e33c022ded507a3bd0ae9
Raja Kolli <rajadilipkolli@gmail.com>
2023-09-06 05:37:29 +0000

    feat : using jupiter Repeated Test instead of JMH

--- api-gateway/src/test/java/com/example/api/gateway/config/RateLimiterConfigurationIntegrationTest.java
+++ api-gateway/src/test/java/com/example/api/gateway/config/RateLimiterConfigurationIntegrationTest.java
@@ -1,21 +1,12 @@
 /* Licensed under Apache-2.0 2023 */
 package com.example.api.gateway.config;
 
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.testcontainers.containers.MockServerContainer;
@@ -23,9 +14,6 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
 @Slf4j
-@State(Scope.Thread)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class RateLimiterConfigurationIntegrationTest extends AbstractIntegrationTest {
 
     @Container
@@ -43,10 +31,7 @@ public class RateLimiterConfigurationIntegrationTest extends AbstractIntegration
         mockServerClient = new MockServerClient(mockServer.getHost(), mockServer.getServerPort());
     }
 
-    @Test
-    @Benchmark
-    @Warmup(iterations = 50, time = 100, timeUnit = TimeUnit.MILLISECONDS)
-    @Measurement(iterations = 50, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+    @RepeatedTest(value = 25)
     public void testorderService() {
         mockServerClient
                 .when(HttpRequest.request().withPath("/order-service/api/1"))
