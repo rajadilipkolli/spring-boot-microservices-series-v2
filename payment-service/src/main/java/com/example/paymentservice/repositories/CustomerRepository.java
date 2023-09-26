072502b09cdd2a05d2b4a6cec759467228e06e9b
Raja Kolli <rajadilipkolli@gmail.com>
2023-09-26 16:44:44 +0000

    feat: adds dependencies

--- payment-service/src/main/java/com/example/paymentservice/repositories/CustomerRepository.java
+++ payment-service/src/main/java/com/example/paymentservice/repositories/CustomerRepository.java
@@ -1,12 +1,10 @@
 /*** Licensed under MIT License Copyright (c) 2022-2023 Raja Kolli. ***/
 package com.example.paymentservice.repositories;
 
-import com.example.paymentservice.entities.Customer;
 import com.example.paymentservice.model.response.CustomerResponse;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository {
 
     Optional<CustomerResponse> findByName(String name);
 }
