d42923d8a4acd95dd826c63d5df0e52d0fd20af5
Raja Kolli <rajadilipkolli@gmail.com>
2023-09-26 17:00:25 +0000

    adds used methods

--- payment-service/src/main/java/com/example/paymentservice/repositories/CustomerRepository.java
+++ payment-service/src/main/java/com/example/paymentservice/repositories/CustomerRepository.java
@@ -1,10 +1,26 @@
 /*** Licensed under MIT License Copyright (c) 2022-2023 Raja Kolli. ***/
 package com.example.paymentservice.repositories;
 
+import com.example.paymentservice.entities.Customer;
 import com.example.paymentservice.model.response.CustomerResponse;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CustomerRepository {
 
     Optional<CustomerResponse> findByName(String name);
+
+    Optional<Customer> findById(Long customerId);
+
+    Customer save(Customer customer);
+
+    void deleteById(Long id);
+
+    Page<Customer> findAll(Pageable pageable);
+
+    List<Customer> saveAll(List<Customer> customerList);
+
+    void deleteAll();
 }
