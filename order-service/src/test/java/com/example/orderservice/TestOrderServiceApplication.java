df128bd83ffc8105d08b89f32e7f6ad87e519edc
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-29 23:15:19 +0000

    feat : upgrade to spring boot 3.4.x

--- order-service/src/test/java/com/example/orderservice/TestOrderServiceApplication.java
+++ order-service/src/test/java/com/example/orderservice/TestOrderServiceApplication.java
@@ -6,43 +6,19 @@
 
 package com.example.orderservice;
 
+import com.example.orderservice.common.ContainersConfig;
 import com.example.orderservice.common.PostGreSQLContainer;
 import com.example.orderservice.utils.AppConstants;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.devtools.restart.RestartScope;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
 @ImportTestcontainers(PostGreSQLContainer.class)
 public class TestOrderServiceApplication {
-
-    @Bean
-    @ServiceConnection
-    @RestartScope
-    KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka").withTag("7.6.1"))
-                .withKraft()
-                .withReuse(true);
-    }
-
-    @Bean
-    @ServiceConnection(name = "openzipkin/zipkin")
-    GenericContainer<?> zipkinContainer() {
-        return new GenericContainer<>(DockerImageName.parse("openzipkin/zipkin:latest"))
-                .withExposedPorts(9411)
-                .withReuse(true);
-    }
 
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", AppConstants.PROFILE_LOCAL);
         SpringApplication.from(OrderServiceApplication::main)
-                .with(TestOrderServiceApplication.class)
+                .with(ContainersConfig.class)
                 .run(args);
     }
 }
---
+++ payment-service/src/test/java/com/example/paymentservice/common/SQLContainerConfig.java
@@ -0,0 +1,19 @@
+/*** Licensed under MIT License Copyright (c) 2024 Raja Kolli. ***/
+package com.example.paymentservice.common;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class SQLContainerConfig {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("17-alpine"))
+                .withReuse(true);
+    }
+}
