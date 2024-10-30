d4dc6fd104e6a7cf75afd02ff7eb1e4d8feb0207
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-30 10:16:05 +0000

    improvise

--- inventory-service/src/test/java/com/example/inventoryservice/TestInventoryApplication.java
+++ inventory-service/src/test/java/com/example/inventoryservice/TestInventoryApplication.java
@@ -9,15 +9,13 @@ package com.example.inventoryservice;
 import com.example.inventoryservice.config.NonSQLContainersConfig;
 import com.example.inventoryservice.config.SQLContainersConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Import;
 
-@Import(SQLContainersConfig.class)
 public class TestInventoryApplication {
 
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "test");
         SpringApplication.from(InventoryServiceApplication::main)
-                .with(NonSQLContainersConfig.class)
+                .with(NonSQLContainersConfig.class, SQLContainersConfig.class)
                 .run(args);
     }
 }
