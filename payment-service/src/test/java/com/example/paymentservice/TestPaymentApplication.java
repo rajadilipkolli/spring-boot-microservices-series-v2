e9d40d065035d29c211702b33caf7a95916384ba
Raja Kolli <rajadilipkolli@gmail.com>
2024-11-01 09:53:18 +0000

    feat : use newly introduced additional profiles

--- payment-service/src/test/java/com/example/paymentservice/TestPaymentApplication.java
+++ payment-service/src/test/java/com/example/paymentservice/TestPaymentApplication.java
@@ -3,14 +3,15 @@ package com.example.paymentservice;
 
 import com.example.paymentservice.common.NonSQLContainerConfig;
 import com.example.paymentservice.common.SQLContainerConfig;
+import com.example.paymentservice.utils.AppConstants;
 import org.springframework.boot.SpringApplication;
 
 public class TestPaymentApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active", "local");
         SpringApplication.from(PaymentApplication::main)
                 .with(SQLContainerConfig.class, NonSQLContainerConfig.class)
+                .withAdditionalProfiles(AppConstants.PROFILE_LOCAL)
                 .run(args);
     }
 }
