e75c814e1afb58d8239dd11baa0cb16ec882a545
Raja Kolli <rajadilipkolli@gmail.com>
2024-11-15 20:14:40 +0530

    feat : upgrade to spring boot 3.4.0-RC1 (#886)
    * feat : upgrade to spring boot 3.4.0-RC1
    
    * downgrade kafka as there is an issue

---
+++ catalog-service/src/test/java/com/example/catalogservice/common/SQLContainerConfig.java
@@ -0,0 +1,25 @@
+/***
+<p>
+    Licensed under MIT License Copyright (c) 2024 Raja Kolli.
+</p>
+***/
+
+package com.example.catalogservice.common;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class SQLContainerConfig {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("17.1-alpine"))
+                .withDatabaseName("catalog-service")
+                .withReuse(true);
+    }
+}
