08f8b5e51b5b8d5b2f8d26d2a84948a003777e30
Raja Kolli <rajadilipkolli@gmail.com>
2025-05-11 14:41:41 +0530

    fix : issue with loading data in UI (#1046)

--- retail-store-webapp/src/main/resources/static/js/products.js
+++ retail-store-webapp/src/main/resources/static/js/products.js
@@ -1,21 +1,57 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('initData', (pageNo) => ({
-        pageNo: pageNo,
+    Alpine.data('initData', () => ({
+        pageNo: 0,
         products: {
-            data: []
+            data: [],
+            totalPages: 0,
+            totalElements: 0,
+            pageNumber: 0,
+            isFirst: true,
+            isLast: false,
+            hasNext: false,
+            hasPrevious: false
         },
         init() {
-            this.loadProducts(this.pageNo);
+            this.loadProducts();
             updateCartItemCount();
         },
-        loadProducts(pageNo) {
-            $.getJSON("/api/products?page="+pageNo, (resp)=> {
-                console.log("Products Resp:", resp)
-                this.products = resp;
-            });
+        loadProducts() {
+            fetch(`/api/products?page=${this.pageNo}`)
+                .then(response => response.json())
+                .then(data => {
+                    this.products = data;
+                    this.pageNo = data.pageNumber - 1;
+                })
+                .catch(error => console.error('Error loading products:', error));
         },
         addToCart(product) {
             addProductToCart(product)
+        },
+        goToPage(pageNo) {
+            if (pageNo >= 0 && pageNo < this.products.totalPages) {
+                this.pageNo = pageNo;
+                this.loadProducts();
+            }
+        },
+        nextPage() {
+            if (this.products.hasNext) {
+                this.goToPage(this.products.pageNumber);
+            }
+        },
+        previousPage() {
+            if (this.products.hasPrevious) {
+                this.goToPage(this.products.pageNumber - 2);
+            }
+        },
+        firstPage() {
+            if (!this.products.isFirst) {
+                this.goToPage(0);
+            }
+        },
+        lastPage() {
+            if (!this.products.isLast) {
+                this.goToPage(this.products.totalPages - 1);
+            }
         }
     }))
 });
