0f7c542f9b051f9f12bf3de8e98a272ffee0df1d
Raja Kolli <rajadilipkolli@gmail.com>
2025-05-13 07:36:13 +0000

    feat : adds more junit tests

---
+++ api-gateway/src/test/java/com/example/api/gateway/config/WebFluxConfigIntegrationTest.java
@@ -0,0 +1,69 @@
+/***
+<p>
+    Licensed under MIT License Copyright (c) 2025 Raja Kolli.
+</p>
+***/
+
+package com.example.api.gateway.config;
+
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static org.springframework.http.HttpHeaders.ORIGIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+
+class WebFluxConfigIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void shouldAllowCorsForConfiguredOrigin() {
+        webTestClient
+                .options()
+                .uri("/test-path")
+                .header(ORIGIN, "http://localhost:8765")
+                .header("Access-Control-Request-Method", HttpMethod.GET.name())
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .valueEquals(ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:8765")
+                .expectHeader()
+                .valueEquals(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+    }
+
+    @Test
+    void shouldNotAllowCorsForUnconfiguredOrigin() {
+        webTestClient
+                .options()
+                .uri("/test-path")
+                .header(ORIGIN, "http://unknown-origin:8080")
+                .header("Access-Control-Request-Method", HttpMethod.GET.name())
+                .exchange()
+                .expectStatus()
+                .isForbidden();
+    }
+
+    @Test
+    void shouldAllowConfiguredMethods() {
+        webTestClient
+                .options()
+                .uri("/test-path")
+                .header(ORIGIN, "http://localhost:8765")
+                .header("Access-Control-Request-Method", HttpMethod.PUT.name())
+                .exchange()
+                .expectStatus()
+                .isOk();
+    }
+
+    @Test
+    void shouldNotAllowUnconfiguredMethods() {
+        webTestClient
+                .options()
+                .uri("/test-path")
+                .header(ORIGIN, "http://localhost:8765")
+                .header("Access-Control-Request-Method", HttpMethod.DELETE.name())
+                .exchange()
+                .expectStatus()
+                .isForbidden();
+    }
+}
