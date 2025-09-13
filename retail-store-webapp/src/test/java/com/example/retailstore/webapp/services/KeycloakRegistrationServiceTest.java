1a7a66ddb1916dcf33f7ae55ac43265dbbd521de
Raja Kolli <rajadilipkolli@gmail.com>
2025-09-13 14:02:28 +0000

    spotless fix

--- retail-store-webapp/src/test/java/com/example/retailstore/webapp/services/KeycloakRegistrationServiceTest.java
+++ retail-store-webapp/src/test/java/com/example/retailstore/webapp/services/KeycloakRegistrationServiceTest.java
@@ -7,9 +7,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.atLeastOnce;
-
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
