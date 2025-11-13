ae54058062bc89b76095896f9bab18419f92d60b
Raja Kolli <rajadilipkolli@gmail.com>
2025-11-13 17:53:41 +0530

    feat : Upgrade to spring boot 4.0.0-RC2 (#1276)

--- catalog-service/src/test/java/com/example/catalogservice/common/SQLContainerConfig.java
+++ catalog-service/src/test/java/com/example/catalogservice/common/SQLContainerConfig.java
@@ -9,7 +9,7 @@ package com.example.catalogservice.common;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -17,8 +17,8 @@ public class SQLContainerConfig {
 
     @Bean
     @ServiceConnection
-    PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("18-alpine"))
+    PostgreSQLContainer postgreSQLContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres").withTag("18-alpine"))
                 .withDatabaseName("catalog-service")
                 .withReuse(true);
     }
