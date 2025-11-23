a8a83bedc976800a77d9759dc1d7a7bd42b4f0ab
Raja Kolli <rajadilipkolli@gmail.com>
2025-11-23 06:52:47 +0000

    polish

--- catalog-service/src/test/java/com/example/catalogservice/common/ContainersConfig.java
+++ catalog-service/src/test/java/com/example/catalogservice/common/ContainersConfig.java
@@ -22,7 +22,7 @@ public class ContainersConfig {
     @ServiceConnection
     @RestartScope
     KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("apache/kafka-native").withTag("4.1.0"))
+        return new KafkaContainer(DockerImageName.parse("apache/kafka-native").withTag("4.1.1"))
                 .withReuse(true);
     }
 
