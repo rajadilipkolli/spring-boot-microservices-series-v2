8482b28cfba7d010b4a83b568d1e28fdd0da2a5b
Raja Kolli <rajadilipkolli@gmail.com>
2025-12-07 21:59:42 +0530

    polish : using modern http proxy client (#1323)
    * polish : using modern http porxy client
    
    * spotless format fix
    
    * fix : test failures
    
    * fix : issue with proxy

--- order-service/src/main/java/com/example/orderservice/config/HttpClientConfig.java
+++ order-service/src/main/java/com/example/orderservice/config/HttpClientConfig.java
@@ -8,48 +8,23 @@ package com.example.orderservice.config;
 
 import com.example.orderservice.services.CatalogServiceProxy;
 import io.micrometer.observation.ObservationRegistry;
-import org.springframework.boot.restclient.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.support.RestClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
+import org.springframework.web.service.registry.ImportHttpServices;
 
 @Configuration(proxyBeanMethods = false)
+@ImportHttpServices(CatalogServiceProxy.class)
 class HttpClientConfig {
 
-    private final ApplicationProperties applicationProperties;
-
-    HttpClientConfig(ApplicationProperties applicationProperties) {
-        this.applicationProperties = applicationProperties;
-    }
-
     @Bean
-    RestClientCustomizer restClientCustomizer(ObservationRegistry observationRegistry) {
-        return restClientBuilder ->
-                restClientBuilder
-                        .defaultHeaders(
-                                httpHeaders -> {
-                                    httpHeaders.add(
-                                            HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-                                    httpHeaders.add(
-                                            HttpHeaders.CONTENT_TYPE,
-                                            MediaType.APPLICATION_JSON_VALUE);
-                                })
-                        .baseUrl(applicationProperties.catalogServiceUrl())
-                        .observationRegistry(observationRegistry);
-    }
-
-    @Bean
-    HttpServiceProxyFactory httpServiceProxyFactory(RestClient.Builder builder) {
-        RestClient restClient = builder.build();
-        return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
-    }
-
-    @Bean
-    CatalogServiceProxy catalogServiceProxy(HttpServiceProxyFactory httpServiceProxyFactory) {
-        return httpServiceProxyFactory.createClient(CatalogServiceProxy.class);
+    RestClientHttpServiceGroupConfigurer groupConfigurer(
+            ObservationRegistry observationRegistry, ApplicationProperties applicationProperties) {
+        return groups ->
+                groups.forEachClient(
+                        (_, builder) ->
+                                builder.baseUrl(applicationProperties.catalogServiceUrl())
+                                        .observationRegistry(observationRegistry)
+                                        .build());
     }
 }
--- order-service/src/main/java/com/example/orderservice/services/CatalogServiceProxy.java
+++ order-service/src/main/java/com/example/orderservice/services/CatalogServiceProxy.java
@@ -8,12 +8,17 @@ package com.example.orderservice.services;
 
 import com.example.orderservice.config.logging.Loggable;
 import java.util.List;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
 
 // @HttpExchange("lb://catalog-service/")
 // @HttpExchange("http://localhost:18080/catalog-service")
-// @HttpExchange(url = "${application.catalog-service-url}")
+@HttpExchange(
+        // url = "${application.catalog-service-url}",
+        accept = MediaType.APPLICATION_JSON_VALUE,
+        contentType = MediaType.APPLICATION_JSON_VALUE)
 @Loggable
 public interface CatalogServiceProxy {
 
