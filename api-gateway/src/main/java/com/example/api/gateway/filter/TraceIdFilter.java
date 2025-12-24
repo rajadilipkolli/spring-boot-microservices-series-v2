e85c0205308d1cc8df45ef7517ae1bb9e16c1505
Raja Kolli <rajadilipkolli@gmail.com>
2025-12-24 12:17:21 +0000

    fix : issue

--- api-gateway/src/main/java/com/example/api/gateway/filter/TraceIdFilter.java
+++ api-gateway/src/main/java/com/example/api/gateway/filter/TraceIdFilter.java
@@ -9,6 +9,7 @@ package com.example.api.gateway.filter;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.TraceContext;
 import io.micrometer.tracing.Tracer;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -17,6 +18,7 @@ import reactor.core.publisher.Mono;
 
 // Add Trace ID to HTTP Response Headers (WebFlux)
 @Component
+@Order(2)
 class TraceIdFilter implements WebFilter {
 
     private final Tracer tracer;
@@ -27,10 +29,15 @@ class TraceIdFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        String traceId = getTraceId();
-        if (traceId != null) {
-            exchange.getResponse().getHeaders().add("X-Trace-Id", traceId);
-        }
+        exchange.getResponse()
+                .beforeCommit(
+                        () -> {
+                            String traceId = getTraceId();
+                            if (traceId != null) {
+                                exchange.getResponse().getHeaders().add("X-Trace-Id", traceId);
+                            }
+                            return Mono.empty();
+                        });
         return chain.filter(exchange);
     }
 
