8be365e6140df9e37ccd515af863824375353baa
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2026-01-23 15:31:51 +0000

    upgrade version in tests

--- order-service/src/test/java/com/example/orderservice/common/ContainersConfig.java
+++ order-service/src/test/java/com/example/orderservice/common/ContainersConfig.java
@@ -30,7 +30,7 @@ public class ContainersConfig {
     @ServiceConnection
     @RestartScope
     LgtmStackContainer lgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.15.0"))
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.16.0"))
                 .withStartupTimeout(Duration.ofMinutes(2));
     }
 }
