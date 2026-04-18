ff25b9edae234ccfb92dad9b250c79914e495cc9
Raja Kolli <rajadilipkolli@gmail.com>
2026-04-18 13:53:12 +0530

    upgrade in tests

--- catalog-service/src/test/java/com/example/catalogservice/common/ContainersConfig.java
+++ catalog-service/src/test/java/com/example/catalogservice/common/ContainersConfig.java
@@ -30,7 +30,7 @@ public class ContainersConfig {
     @Bean
     @ServiceConnection
     LgtmStackContainer lgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.24.1"))
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.25.0"))
                 .withStartupTimeout(Duration.ofMinutes(2));
     }
 
