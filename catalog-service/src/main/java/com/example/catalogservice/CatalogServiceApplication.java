af5154d295a5051cd7a8f953f41f9fb3897c1e6d
Raja Kolli <rajadilipkolli@gmail.com>
2026-04-18 15:19:05 +0000

    revert : native build

--- catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -7,15 +7,12 @@
 package com.example.catalogservice;
 
 import com.example.catalogservice.config.ApplicationProperties;
-import com.example.catalogservice.config.CatalogServiceRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ApplicationProperties.class})
-@ImportRuntimeHints(CatalogServiceRuntimeHints.class)
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
