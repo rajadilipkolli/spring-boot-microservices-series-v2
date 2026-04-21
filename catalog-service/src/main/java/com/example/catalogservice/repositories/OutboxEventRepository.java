d785454edb634481c6f2c872d1a691d72fa3a278
Raja Kolli <rajadilipkolli@gmail.com>
2026-04-21 14:41:45 +0000

    fix spotless formatting issue

--- catalog-service/src/main/java/com/example/catalogservice/repositories/OutboxEventRepository.java
+++ catalog-service/src/main/java/com/example/catalogservice/repositories/OutboxEventRepository.java
@@ -54,5 +54,6 @@ public interface OutboxEventRepository extends ReactiveCrudRepository<OutboxEven
 
     Mono<Long> countByStatus(OutboxEventStatus status);
 
-    Mono<Integer> deleteAllByStatusAndCreatedAtBefore(OutboxEventStatus status, OffsetDateTime time);
+    Mono<Integer> deleteAllByStatusAndCreatedAtBefore(
+            OutboxEventStatus status, OffsetDateTime time);
 }
