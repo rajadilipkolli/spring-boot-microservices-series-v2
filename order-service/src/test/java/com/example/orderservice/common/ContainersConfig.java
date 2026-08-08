596a06f4223fc6a8884e8cf64652e601c7edd61b
Raja Kolli <rajadilipkolli@gmail.com>
2026-08-08 08:37:59 +0530

    upgrade in tests

--- order-service/src/test/java/com/example/orderservice/common/ContainersConfig.java
+++ order-service/src/test/java/com/example/orderservice/common/ContainersConfig.java
@@ -30,7 +30,7 @@ public class ContainersConfig {
     @ServiceConnection
     @RestartScope
     LgtmStackContainer lgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.29.2"))
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.30.1"))
                 .withStartupTimeout(Duration.ofMinutes(2));
     }
 }
