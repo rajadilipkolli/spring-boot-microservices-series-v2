b20273737ed93f06ec0f11a059ab4b4389662ff5
Raja Kolli <rajadilipkolli@gmail.com>
2026-08-08 19:37:07 +0530

    feat : move to opentelemetry agent (#1621)
    * feat : move to opentelemetry agent
    
    * attempt to fix test failure
    
    * attempt to fix issue with jacoco
    
    * attempt to fix
    
    * fix issue with jacoco
    
    * upgrade redis version in test

--- catalog-service/src/test/java/com/example/catalogservice/common/ContainersConfig.java
+++ catalog-service/src/test/java/com/example/catalogservice/common/ContainersConfig.java
@@ -6,13 +6,11 @@
 
 package com.example.catalogservice.common;
 
-import java.time.Duration;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistrar;
-import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -25,13 +23,6 @@ public class ContainersConfig {
     KafkaContainer kafkaContainer() {
         return new KafkaContainer(DockerImageName.parse("apache/kafka-native").withTag("4.3.1"))
                 .withReuse(true);
-    }
-
-    @Bean
-    @ServiceConnection
-    LgtmStackContainer lgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.30.1"))
-                .withStartupTimeout(Duration.ofMinutes(2));
     }
 
     @Bean
