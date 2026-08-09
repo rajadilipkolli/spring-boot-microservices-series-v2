feb694e002adc175d7e6ecdd411999c7ec01d079
Raja Kolli <rajadilipkolli@gmail.com>
2026-08-09 07:06:51 +0000

    refactor : remove unused LGTM Continers
    As we have moved towards the agent based opentelemetry adding explict LGTM container for integration tests is not necessary

--- api-gateway/src/test/java/com/example/api/gateway/config/ContainerConfig.java
+++ api-gateway/src/test/java/com/example/api/gateway/config/ContainerConfig.java
@@ -7,22 +7,13 @@
 package com.example.api.gateway.config;
 
 import com.redis.testcontainers.RedisContainer;
-import java.time.Duration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainerConfig {
-
-    @Bean
-    @ServiceConnection
-    LgtmStackContainer lgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.30.1"))
-                .withStartupTimeout(Duration.ofMinutes(2));
-    }
 
     @Bean
     @ServiceConnection(name = "redis")
